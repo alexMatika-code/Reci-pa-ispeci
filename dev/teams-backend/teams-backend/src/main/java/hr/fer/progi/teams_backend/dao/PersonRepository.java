bc6b7962995122cc8b596fd77d960b458f00e2fd
Milan <milan.vidakovic04@gmail.com>
2024-11-09 12:11:27 +0100

    Added image fetching to SecurityConfig

--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/dao/PersonRepository.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/dao/PersonRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    boolean existsByEmail(String email);
 }
--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/dao/RoleRepository.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/dao/RoleRepository.java
@@ -1,9 +1,11 @@
 package hr.fer.progi.teams_backend.dao;
 
+import hr.fer.progi.teams_backend.constants.Roles;
 import hr.fer.progi.teams_backend.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(Roles roles);
 }
