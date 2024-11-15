8d707544d4f840bee475a76b5e534f3f3b49cca9
alexMatika-code <am54879@fer.hr>
2024-11-15 20:35:56 +0100

    changes in docker

--- dev/teams-frontend/teams-frontend/vite.config.js
+++ dev/teams-frontend/teams-frontend/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://reci-pa-ispeci-q8z2.onrender.com', // Backend URL
-        changeOrigin: true,
-        secure: true, // Since Render uses HTTPS
-      }
-    }
-  }
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/": {
+//         target: "http://localhost:8080",
+//         changeOrigin: true
+//       },
+//     },
+//   },
+// })
