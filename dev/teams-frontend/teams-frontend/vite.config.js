e5976caf5707c422159d1c944b67f84096c68363
FrankoCosic <franko.cosic@fer.unizg.hr>
2024-11-15 20:50:07 +0100

    Modified config for render

--- dev/teams-frontend/teams-frontend/vite.config.js
+++ dev/teams-frontend/teams-frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api/": {
-//         target: "http://localhost:8080",
-//         changeOrigin: true
-//       },
-//     },
-//   },
-// })
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://reci-pa-ispeci-be.onrender.com", // Backend URL
+                changeOrigin: true, // Ensures the host header is updated
+                secure: false, // Disable SSL verification if running HTTP
+            },
+        },
+    },
+});
