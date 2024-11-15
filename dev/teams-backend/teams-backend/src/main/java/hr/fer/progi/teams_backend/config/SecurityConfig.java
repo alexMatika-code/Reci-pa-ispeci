6abc513bdd1c858851bdf24fe269dddfb167a9a1
FrankoCosic <franko.cosic@fer.unizg.hr>
2024-11-15 23:48:34 +0100

    update for local usage, dev profile active

--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/config/SecurityConfig.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 
     @Autowired
     private PersonRepository personRepository;
-    private final String frontendUrl = "https://reci-pa-ispeci.onrender.com/";
+    private final String frontendUrl = "http://localhost:3000";
 
     @Bean
     public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
