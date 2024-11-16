33642c31cd5b4d39ce86f83c204c3d93cf2c32ed
TheMarkTwo <matija.lovrekovic@fer.hr>
2024-11-16 19:06:46 +0100

    Priprema za deploy na Render

--- dev/teams-frontend/teams-frontend/vite.config.js
+++ dev/teams-frontend/teams-frontend/vite.config.js
@@ -6,10 +6,9 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      "/api/": {
         target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        changeOrigin: true
       }
     }
   }
