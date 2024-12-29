f0f8e7be3907e4fc4d5f043ccdbb591b6858b4f6
alexMatika-code <am54879@fer.hr>
2024-12-29 16:22:59 +0100

    removed cors config

--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/config/SecurityConfig.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/config/SecurityConfig.java
@@ -5,35 +5,20 @@ import hr.fer.progi.teams_backend.dao.PersonRepository;
 import hr.fer.progi.teams_backend.dao.RoleRepository;
 import hr.fer.progi.teams_backend.domain.Person;
 import hr.fer.progi.teams_backend.domain.Role;
-import jakarta.servlet.SessionCookieConfig;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.io.IOException;
-import java.util.List;
 
 
 @Configuration
@@ -47,40 +32,22 @@ public class SecurityConfig {
     private PersonRepository personRepository;
     private final String frontendUrl = "https://reci-pa-ispeci.onrender.com";
 
-
     @Bean
     public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
-        http.requiresChannel(channel -> channel
-                .anyRequest().requiresSecure()
-        );
-
-        return http.
-                cors(Customizer.withDefaults()).
-                csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement((ses) -> ses.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-                .sessionManagement((ses) -> ses.sessionFixation().migrateSession())
+        return http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
                     registry.requestMatchers("/").permitAll();
                     registry.requestMatchers("/recipes/public").permitAll();
                     registry.requestMatchers("/people/profile/{username}").permitAll();
-                    registry.requestMatchers("/login").permitAll();
-                    registry.requestMatchers("/ingredients").permitAll();
-                    registry.requestMatchers("/ingredients/{id}").permitAll();
-                    registry.requestMatchers("/oauth2/authorization/google").permitAll();
                     registry.anyRequest().authenticated();
                 })
                 .oauth2Login(oauth2 -> {
-                    oauth2.userInfoEndpoint(
-                                    userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
-                            .successHandler(
-                                    (request, response, authentication) -> {
-                                        response.sendRedirect(frontendUrl);
-                                    });
+                    oauth2
+                            .loginPage("/oauth2/authorization/google")
+                            .successHandler(new CustomAuthenticationSuccessHandler());
                 })
-                .exceptionHandling(handling ->
-                        handling.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-                ).build();
+                .build();
     }
 
     private class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
@@ -96,9 +63,6 @@ public class SecurityConfig {
                 newUser.setLastName(oauth2User.getAttribute("family_name"));
                 newUser.setImage(oauth2User.getAttribute("picture"));
 
-                String firstName = oauth2User.getAttribute("given_name");
-                String lastName = oauth2User.getAttribute("family_name");
-
                 String username = email.contains("@") ? email.substring(0, email.indexOf("@")) : email;
                 newUser.setUsername(username);
 
@@ -113,43 +77,6 @@ public class SecurityConfig {
             }
 
             response.sendRedirect(frontendUrl);
-        }
-    }
-
-    @Bean
-    public GrantedAuthoritiesMapper authorityMapper() {
-        return authorities -> {
-            if (authorities instanceof List) {
-                for (Object authority : authorities) {
-                    if (authority instanceof DefaultOidcUser oidcUser) {
-                        handleNewUser(oidcUser); // Create user if they don't exist
-                    }
-                }
-            }
-            return authorities;
-        };
-    }
-
-    private void handleNewUser(OidcUser oidcUser) {
-        String email = oidcUser.getAttribute("email");
-        if (!personRepository.existsByEmail(email)) {
-            Person newUser = new Person();
-            newUser.setEmail(email);
-            newUser.setFirstName(oidcUser.getAttribute("given_name"));
-            newUser.setLastName(oidcUser.getAttribute("family_name"));
-            newUser.setImage(oidcUser.getAttribute("picture"));
-
-            String username = email.contains("@") ? email.substring(0, email.indexOf("@")) : email;
-            newUser.setUsername(username);
-
-            Role role = roleRepository.findByName(Roles.USER);
-            if (role == null) {
-                role = new Role();
-                role.setName(Roles.USER);
-                roleRepository.save(role);
-            }
-            newUser.setRole(role);
-            personRepository.save(newUser);
         }
     }
 }
