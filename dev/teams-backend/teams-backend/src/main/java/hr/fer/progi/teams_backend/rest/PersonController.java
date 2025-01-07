59dc307af94085c21996c3614436639d2d0260bf
FrankoCosic <franko.cosic@fer.unizg.hr>
2025-01-07 23:03:41 +0100

    added profile to PersonController

--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/rest/PersonController.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/rest/PersonController.java
@@ -12,10 +12,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 import java.util.Objects;
 
+@Profile({"form-security", "oauth-security"})
 @Slf4j
 @RestController
 @RequestMapping("/people")
