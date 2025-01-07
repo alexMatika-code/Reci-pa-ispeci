6e69e455dbb415d861381dba65d2fa7bb9d0b603
alexMatika-code <am54879@fer.hr>
2025-01-07 15:11:48 +0100

    post login

--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/rest/TestController.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/rest/TestController.java
@@ -2,6 +2,7 @@ package hr.fer.progi.teams_backend.rest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 public class TestController {
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public RedirectView redirectToGoogleLogin() {
         log.info("Login started, redirect to google login");
         return new RedirectView("/api/oauth2/authorization/google");
