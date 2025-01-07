27ce8d80ae1dc4c6510ab4edd847b9db8c1ed07a
alexMatika-code <am54879@fer.hr>
2025-01-07 15:51:17 +0100

    post login and free endpoint

--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/rest/TestController.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/rest/TestController.java
@@ -1,6 +1,7 @@
 package hr.fer.progi.teams_backend.rest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,10 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 public class TestController {
 
-    @PostMapping("/login")
-    public RedirectView redirectToGoogleLogin() {
-        log.info("Login started, redirect to google login");
-        return new RedirectView("/api/oauth2/authorization/google");
+    @PostMapping("/api/login")
+    public ResponseEntity<String> handleLoginRequest() {
+        // Respond with the URL for Google OAuth2
+        String googleLoginUrl = "/oauth2/authorization/google";
+        return ResponseEntity.ok(googleLoginUrl);
     }
 }
