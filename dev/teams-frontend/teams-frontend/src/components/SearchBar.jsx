1aed9013ba5829151ae355e14fb3d5d5e5bc2436
Matija Lovrekovic <matija.lovrekovic@fer.hr>
2025-01-09 18:38:52 +0100

    Update SearchBar.jsx

--- dev/teams-frontend/teams-frontend/src/components/SearchBar.jsx
+++ dev/teams-frontend/teams-frontend/src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ import InputGroupText from "react-bootstrap/InputGroupText";
 const SearchBar = ({showModal, setQuery, query, setTimeToCook, timeToCook, search}) => {
     return (
         <Row className={"w-60 mx-auto py-5 d-flex justify-content-center align-items-center"}>
-            <div className={"col-7"}>
+            <div className={"col-xl-7 col-lg-6 col-sm-12 mb-2"}>
                 <InputGroup>
                     <InputGroupText><BsSearch /></InputGroupText>
                     <Form.Control type="text"
@@ -16,7 +16,7 @@ const SearchBar = ({showModal, setQuery, query, setTimeToCook, timeToCook, searc
                                   onChange={(e) => setQuery(e.target.value)}/>
                 </InputGroup>
             </div>
-            <div className={"col-3"}>
+            <div className={"col-xl-3 col-lg-4 col-sm-12 mb-2"}>
                 <InputGroup>
                     <InputGroupText><BsClock /></InputGroupText>
                     <Form.Control type="text"
@@ -26,15 +26,12 @@ const SearchBar = ({showModal, setQuery, query, setTimeToCook, timeToCook, searc
                                   onChange={(e) => setTimeToCook(e.target.value)}/>
                 </InputGroup>
             </div>
-            <div className={"col-1"}>
-                <Button variant="danger" className={"rounded-circle searchbar-button d-flex align-items-center"} onClick={showModal}>
+            <div className={"col-md-2 px-20 d-flex justify-content-center align-items-center"}>
+                <Button variant="danger" className={"rounded-circle searchbar-button d-flex align-items-center m-3"} onClick={showModal}>
                     <BsBasket2Fill className={"font-2rem"}/>
                 </Button>
-            </div>
-
-            <div className={"col-1"}>
                 <Button variant="success"
-                        className={"rounded-circle searchbar-button d-flex align-items-center"} onClick={search}>
+                        className={"rounded-circle searchbar-button d-flex align-items-center m-3"} onClick={search}>
                     <BsFillSendFill className={"font-2rem"}/>
                 </Button>
             </div>
