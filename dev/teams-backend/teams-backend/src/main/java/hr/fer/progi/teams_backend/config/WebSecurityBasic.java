3818b01b688d577e680095e40bdd74a236ae85e5
alexMatika-code <am54879@fer.hr>
2025-01-11 17:33:39 +0100

    authorities and WebSocketConfig

--- dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/config/WebSecurityBasic.java
+++ dev/teams-backend/teams-backend/src/main/java/hr/fer/progi/teams_backend/config/WebSecurityBasic.java
@@ -8,20 +8,22 @@ import hr.fer.progi.teams_backend.domain.Role;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -30,14 +32,11 @@ import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import java.util.*;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = false)
 public class WebSecurityBasic {
-
     @Autowired
     private RoleRepository roleRepository;
 
@@ -50,12 +49,11 @@ public class WebSecurityBasic {
     @Bean
     @Profile("basic-security")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .formLogin(withDefaults())
-                .httpBasic(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
     @Bean
@@ -63,102 +61,83 @@ public class WebSecurityBasic {
     public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/recipes/public",
-                                "/",
-                                "/ingredients",
-                                "/people/profile/{username}",
-                                "/recipes/{recipeId}",
-                                "/ingredients/recipe/{recipeId}"
-                        ).permitAll()
-                        .anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2
-                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
-                                .userAuthoritiesMapper(this.authorityMapper()))
-                        .successHandler((request, response, authentication) -> {
-                            OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
-                            String email = oauth2User.getAttribute("email");
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/recipes/public", "/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(oauth2 -> {
+                    oauth2
+                            .userInfoEndpoint(
+                                    userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
+                            .successHandler(
+                                    (request, response, authentication) -> {
+                                        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+                                        String email = oauth2User.getAttribute("email");
 
-                            if (!personRepository.existsByEmail(email)) {
-                                Person newUser = new Person();
-                                newUser.setEmail(email);
-                                newUser.setFirstName(oauth2User.getAttribute("given_name"));
-                                newUser.setLastName(oauth2User.getAttribute("family_name"));
-                                newUser.setImage(oauth2User.getAttribute("picture"));
 
-                                String username = email.contains("@")
-                                        ? email.substring(0, email.indexOf("@"))
-                                        : email;
-                                newUser.setUsername(username);
+                                        if (!personRepository.existsByEmail(email)) {
+                                            Person newUser = new Person();
+                                            newUser.setEmail(email);
+                                            newUser.setFirstName(oauth2User.getAttribute("given_name"));
+                                            newUser.setLastName(oauth2User.getAttribute("family_name"));
+                                            newUser.setImage(oauth2User.getAttribute("picture"));
 
-                                Role role = roleRepository.findByName(Roles.USER);
-                                if (role == null) {
-                                    role = new Role();
-                                    role.setName(Roles.USER);
-                                    roleRepository.save(role);
-                                }
-                                newUser.setRole(role);
-                                personRepository.save(newUser);
-                            }
+                                            String username = email.contains("@") ? email.substring(0, email.indexOf("@")) : email;
+                                            newUser.setUsername(username);
 
-                            response.sendRedirect(frontendUrl);
-                        }))
-                .exceptionHandling(handling -> handling
-                        .authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
+
+                                            Role role = roleRepository.findByName(Roles.USER);
+                                            if (role == null) {
+                                                role = new Role();
+                                                role.setName(Roles.USER);
+                                                roleRepository.save(role);
+                                            }
+                                            newUser.setRole(role);
+                                            personRepository.save(newUser);
+                                        }
+
+                                        response.sendRedirect(frontendUrl);
+                                    });
+                })
+                .exceptionHandling(handling -> handling.authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
                 .build();
     }
 
     @Bean
     @Profile("form-security")
     public SecurityFilterChain spaFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(configurer -> configurer
-                        .successHandler((request, response, authentication) ->
-                                response.setStatus(HttpStatus.NO_CONTENT.value()))
-                        .failureHandler(new SimpleUrlAuthenticationFailureHandler()))
-                .exceptionHandling(configurer -> {
-                    final RequestMatcher matcher = new NegatedRequestMatcher(
-                            new MediaTypeRequestMatcher(MediaType.TEXT_HTML));
-                    configurer.defaultAuthenticationEntryPointFor(
-                            (request, response, authException) -> response.setStatus(HttpServletResponse.SC_UNAUTHORIZED),
-                            matcher);
-                })
-                .logout(configurer -> configurer
-                        .logoutUrl("/logout")
-                        .logoutSuccessHandler((request, response, authentication) ->
-                                response.setStatus(HttpStatus.NO_CONTENT.value())))
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
+                .anyRequest().authenticated());
+        http.formLogin(configurer -> {
+                    configurer.successHandler((request, response, authentication) ->
+                                    response.setStatus(HttpStatus.NO_CONTENT.value())
+                            )
+                            .failureHandler(new SimpleUrlAuthenticationFailureHandler());
+                }
+        );
+        http.exceptionHandling(configurer -> {
+            final RequestMatcher matcher = new NegatedRequestMatcher(
+                    new MediaTypeRequestMatcher(MediaType.TEXT_HTML));
+            configurer
+                    .defaultAuthenticationEntryPointFor((request, response, authException) -> {
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    }, matcher);
+        });
+        http.logout(configurer -> configurer
+                .logoutUrl("/logout")
+                .logoutSuccessHandler((request, response, authentication) ->
+                        response.setStatus(HttpStatus.NO_CONTENT.value())));
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
     private GrantedAuthoritiesMapper authorityMapper() {
-        return (authorities) -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+        final SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
 
-            authorities.forEach(authority -> {
-                if (authority instanceof OAuth2UserAuthority) {
-                    OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
+        authorityMapper.setDefaultAuthority("USER");
 
-                    Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
-                    String email = (String) userAttributes.get("email");
-
-                    // Find the person by email
-                    Optional<Person> person = personRepository.findByEmail(email);
-
-                    // Map the user's role to a granted authority
-                    person.ifPresentOrElse(
-                            p -> mappedAuthorities.add(new SimpleGrantedAuthority(p.getRole().toString())),
-                            () -> mappedAuthorities.add(new SimpleGrantedAuthority("USER"))
-                    );
-                }
-            });
-
-            return mappedAuthorities;
-        };
+        return authorityMapper;
     }
 
     @Bean
