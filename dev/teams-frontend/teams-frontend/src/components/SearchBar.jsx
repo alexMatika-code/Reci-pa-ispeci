759ec2828e67b8cf6af960757e0a66ec84128d45
Matija Lovrekovic <matija.lovrekovic@fer.hr>
2025-01-18 12:39:15 +0100

    Update SearchBar.jsx

--- dev/teams-frontend/teams-frontend/src/components/SearchBar.jsx
+++ dev/teams-frontend/teams-frontend/src/components/SearchBar.jsx
@@ -11,33 +11,35 @@ const SearchBar = ({showModal, setQuery, query, setTimeToCook, timeToCook, searc
         <div className={"mb-4"}>
             <Row className={"mt-5"}>
                 <div className={"mx-auto col-xl-5 col-lg-6 col-sm-8 col-10"}>
-                    <div className={"search-bar-container d-flex justify-content-center align-items-center rounded-5 p-2 shadow-sm"}>
+                    <div
+                        className={"search-bar-container d-flex justify-content-center align-items-center rounded-5 p-2 shadow-sm"}>
                         <input type="text"
                                placeholder="U potrazi za dobrim jelom..."
                                className={"p-2 px-4 border-0 w-100 color-dsg search-input font-1-1rem font-weight-600"}
                                value={query}
                                onChange={(e) => setQuery(e.target.value)}/>
-                        <div className={"p-2 search-icon cursor-pointer"} onChange={search}>
+                        <div className={"p-2 search-icon cursor-pointer"} onClick={search}>
                             <BsSearch className={"font-1-8rem color-dsg"}/>
                         </div>
                     </div>
                 </div>
-                <div className={"col-12 d-flex justify-content-center align-items-center pt-2 pb-sm-2 pb-3 color-lsg color-dsg-hover cursor-pointer"}
-                     onClick={() => setShowMenu(!showMenu)}>
-                    <span className={"no-select"}>Filtriranje</span>
-                    {showMenu ? (
-                        <BsChevronUp className={"ml-4 font-1-2rem"}/>
-                    ):(
-                        <BsChevronDown className={"ml-4 font-1-2rem"}/>
-                    )}
-
-                </div>
             </Row>
 
-            <Row className={`${showMenu ? "d-flex" : "d-none"} w-60 mb-5 mx-auto justify-content-center align-items-center responsive-width`}>
+            <span className={"d-flex justify-content-center align-items-center pt-2 pb-sm-2 pb-3 color-lsg color-dsg-hover cursor-pointer"}
+                 onClick={() => setShowMenu(!showMenu)}>
+                <span className={"no-select text-center"}>Filtriranje</span>
+                {showMenu ? (
+                    <BsChevronUp className={"ml-4 font-1-2rem"}/>
+                ) : (
+                    <BsChevronDown className={"ml-4 font-1-2rem"}/>
+                )}
+            </span>
+
+            <Row
+                className={`${showMenu ? "d-flex" : "d-none"} w-60 mb-5 mx-auto justify-content-center align-items-center responsive-width`}>
                 <div className={"col-xl-6 col-lg-7 col-sm-8 px-4"}>
                     <InputGroup>
-                        <InputGroupText><BsClock /></InputGroupText>
+                        <InputGroupText><BsClock/></InputGroupText>
                         <Form.Control type="text"
                                       placeholder="Dostupno vrijeme (minute)"
                                       className={"py-2"}
@@ -46,7 +48,8 @@ const SearchBar = ({showModal, setQuery, query, setTimeToCook, timeToCook, searc
                     </InputGroup>
                 </div>
                 <div className={"col-2 d-flex justify-content-center align-items-center pt-sm-0 pt-2"}>
-                    <Button variant="success" className={"d-flex align-items-center font-1rem rounded-4 px-3"} onClick={showModal}>
+                    <Button variant="success" className={"d-flex align-items-center font-1rem rounded-4 px-3"}
+                            onClick={showModal}>
                         <BsBasket2Fill className={"mr-10"}/>
                         <span className={"font-weight-600"}>Sastojci</span>
                     </Button>
