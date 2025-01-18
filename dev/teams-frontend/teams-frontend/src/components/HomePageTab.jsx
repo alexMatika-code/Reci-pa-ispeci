220e036f97df708599f23f47813e8c344d5bc678
Marta <marta.les@fer.hr>
2025-01-18 19:22:25 +0100

    redesigned the look of recipe cards

--- dev/teams-frontend/teams-frontend/src/components/HomePageTab.jsx
+++ dev/teams-frontend/teams-frontend/src/components/HomePageTab.jsx
@@ -5,11 +5,11 @@ const HomePageTab = ({ showRecommended, setShowRecommended }) => {
 
     return (
         <div className="d-flex justify-content-center align-items-center mb-3">
-            <div className={`mx-3 color-dsg cursor-pointer ${!showRecommended ? "bg-coral text-white bold px-2 py-1 rounded-2" : ""}`}
+            <div className={`mx-3 color-dsg cursor-pointer ${!showRecommended ? "bg-coral text-white bold px-2 py-1 rounded-2 shadow-sm" : ""}`}
                  onClick={() => handleTabClick(false)}>
                 Javno
             </div>
-            <div className={`mx-3 color-dsg cursor-pointer ${showRecommended ? "bg-coral text-white bold px-2 py-1 rounded-2" : ""}`}
+            <div className={`mx-3 color-dsg cursor-pointer ${showRecommended ? "bg-coral text-white bold px-2 py-1 rounded-2 shadow-sm" : ""}`}
                  onClick={() => handleTabClick(true)}>
                 Za tebe
             </div>
