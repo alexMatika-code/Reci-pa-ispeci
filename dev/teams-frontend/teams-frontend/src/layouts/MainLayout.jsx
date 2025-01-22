2e0c3beb5e20186b342159505ad04bb112536b19
Matija Lovrekovic <matija.lovrekovic@fer.hr>
2025-01-22 15:38:06 +0100

    Update MainLayout.jsx

--- dev/teams-frontend/teams-frontend/src/layouts/MainLayout.jsx
+++ dev/teams-frontend/teams-frontend/src/layouts/MainLayout.jsx
@@ -22,7 +22,6 @@ const MainLayout = () => {
                 setCurrentUser(data);
             } catch (error) {
                 console.error("Error fetching currentUser:", error);
-                setCurrentUser(undefined);
             } finally {
                 setLoading(false);
             }
